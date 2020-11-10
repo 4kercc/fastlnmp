fa6bb3c9f5e0a1584bbe4e1cdcb728a18dfbd8be
4kercc <4kercc@users.noreply.github.com>
2020-11-10 09:19:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>恭喜，lnmp搭建成功！</title>
+
+</head>
+<body><center>
+	<div class="container" style="margin-top:9%;">
+  		<div class="jumbotron">
+        <div class="panel panel-success">
+        <div class="panel-heading"><h1>恭喜,lnmp搭建成功！</h1></div>
+        </div>
+          <p><h3>站点创建成功，本页面由系统自动生成。</h3></p>
+          <p><li>本页面于/var/www/html/index.php</li></p>
+          <p><li>您可以删除或覆盖本页面</li></p>
+		  <p><li><a href="http://4ker.cc">刺客博客</a></li></p>
+        </div>
+	</div>
+</body>
+</html>
