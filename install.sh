4cc40f37106ca5fa48c878acffeac691bfcc806e
4kercc <4kercc@users.noreply.github.com>
2020-11-10 09:27:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ yum install -y epel-release epel-release.noarch yum-utils wget
 rpm -Uvh https://github.com/4kercc/fastlnmp/raw/main/remi-release-7.rpm 
 rpm -ivh http://nginx.org/packages/mainline/centos/7/x86_64/RPMS/nginx-1.19.0-1.el7.ngx.x86_64.rpm
 wget -O /etc/yum.repos.d/MariaDB.repo https://github.com/4kercc/fastlnmp/raw/main/MariaDB.repo 
-sed '0,/enabled/{ s/enabled=0/enabled=1/; }' /etc/yum.repos.d/remi-php70.repo > 1.log ;cat 1.log > /etc/yum.repos.d/remi-php70.repo ;rm 1.log -f >/dev/null 2>&1
-yum update -y >/dev/null 2>&1
+sed '0,/enabled/{ s/enabled=0/enabled=1/; }' /etc/yum.repos.d/remi-php70.repo > 1.log ;cat 1.log > /etc/yum.repos.d/remi-php70.repo ;rm 1.log -f 
+yum update -y 
 mkdir -p /var/www/html
 #remi-release-7默认安装php7.0,下面这行设置安装php7.1
 yum-config-manager --enable remi-php71 
